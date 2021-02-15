2ca192e042881ab6adb0ed4c61b73c0448fc0e95
Paulo Sérgio <ps131296@gmail.com>
2021-02-15 15:51:46 -0300

    extracted multiple test cases to a table test

--- user/user_server_test.go
+++ user/user_server_test.go
@@ -11,30 +11,24 @@ import (
 func TestRegister(t *testing.T) {
 	server := UsersServer{}
 
-	t.Run("Delivers 422 status code and missing param error on no body provided", func(t *testing.T) {
+	t.Run("Delivers 422 status code and missing param error correctly", func(t *testing.T) {
 		want := ErrMissingParam("Name, Email, Password, PasswordConfirm")
 		assertMissingParams(t, server, nil, want.Error())
-	})
 
-	t.Run("Delivers 422 status code and missing param error on no params provided", func(t *testing.T) {
-		want0 := ErrMissingParam("Name, Email, Password, PasswordConfirm")
-		assertMissingParams(t, server, strings.NewReader(""), want0.Error())
+		testingTable := []struct {
+			body string
+			want ErrMissingParam
+		}{
+			{"", ErrMissingParam("Name, Email, Password, PasswordConfirm")},
+			{`{"email": "email@mail.com", "password": "password123", "passwordConfirm": "password123"}`, ErrMissingParam("Name")},
+			{`{"name":"any-name", "password": "password123", "passwordConfirm": "password123"}`, ErrMissingParam("Email")},
+			{`{"name":"any-name", "email": "email@mail.com", "passwordConfirm": "password123"}`, ErrMissingParam("Password")},
+			{`{"name":"any-name", "email": "email@mail.com", "password": "password123"}`, ErrMissingParam("PasswordConfirm")},
+		}
 
-		body := `{"email": "email@mail.com", "password": "password123", "passwordConfirm": "password123"}`
-		want := ErrMissingParam("Name")
-		assertMissingParams(t, server, strings.NewReader(body), want.Error())
-
-		body1 := `{"name":"any-name", "password": "password123", "passwordConfirm": "password123"}`
-		want1 := ErrMissingParam("Email")
-		assertMissingParams(t, server, strings.NewReader(body1), want1.Error())
-
-		body2 := `{"name":"any-name", "email": "email@mail.com", "passwordConfirm": "password123"}`
-		want2 := ErrMissingParam("Password")
-		assertMissingParams(t, server, strings.NewReader(body2), want2.Error())
-
-		body3 := `{"name":"any-name", "email": "email@mail.com", "password": "password123"}`
-		want3 := ErrMissingParam("PasswordConfirm")
-		assertMissingParams(t, server, strings.NewReader(body3), want3.Error())
+		for _, testCase := range testingTable {
+			assertMissingParams(t, server, strings.NewReader(testCase.body), testCase.want.Error())
+		}
 	})
 }
 
