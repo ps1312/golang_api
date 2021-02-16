70e1d0f56405b14813c3b15a84e355c0140c0fa6
Paulo Sérgio <ps131296@gmail.com>
2021-02-16 20:50:58 -0300

    changing package to singulr

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"api/food"
+	"api/user"
 	"net/http"
 )
 
 func main() {
-	http.ListenAndServe(":5000", &food.FoodsServer{Store: &food.InMemoryFoodsStore{Foods: []food.Food{}}})
+	http.Handle("/foods", &food.FoodsServer{Store: &food.InMemoryFoodsStore{Foods: []food.Food{}}})
+	http.Handle("/users", &user.Server{Encrypter: &user.BCryptEncrypter{}, Store: &user.InMemoryUsersStore{Users: []user.DatabaseModel{}}})
+	http.ListenAndServe(":5000", nil)
 }
