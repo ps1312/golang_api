07ad25ca484ae258b4643917aac94aa700b4c549
Paulo Sérgio <ps131296@gmail.com>
2021-02-16 21:36:02 -0300

    extracted only in memory storage test to a subtest to give more context

--- user/in_memory_user_store_test.go
+++ user/in_memory_user_store_test.go
@@ -2,13 +2,15 @@ package user
 
 import "testing"
 
-func Test(t *testing.T) {
-	model := DatabaseModel{Name: "any-name", Email: "any@mail.com", password: "any-password"}
-	store := InMemoryUsersStore{}
+func TestInMemoryStore(t *testing.T) {
+	t.Run("Saves user on storage correctly", func(t *testing.T) {
+		model := DatabaseModel{Name: "any-name", Email: "any@mail.com", password: "any-password"}
+		store := InMemoryUsersStore{}
 
-	store.save(model)
+		store.save(model)
 
-	if len(store.Users) != 1 {
-		t.Errorf("got %d, want 1", len(store.Users))
-	}
+		if len(store.Users) != 1 {
+			t.Errorf("got %d, want 1", len(store.Users))
+		}
+	})
 }
